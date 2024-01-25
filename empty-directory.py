071f18a1029f689e6a69af7c543d92043f4752dc
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-01-25 15:08:20 -0700

    Formatting

--- empty-directory.py
+++ empty-directory.py
@@ -7,27 +7,21 @@ from datetime import datetime, timedelta
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Empty directory')
-    parser.add_argument('path', metavar='path', type=str, nargs='+',
-                        help='path to empty')
-    parser.add_argument('--trash', action='store_true',
-                        help='move files to trash')
-    parser.add_argument('--remove', action='store_true',
-                        help='remove files')
-    parser.add_argument('--days', type=int, default=30,
-                        help='days to keep files')
-    parser.add_argument('--start-date', type=str, default=None,
-                        help='start date')
-    parser.add_argument('--dry-run', action='store_true',
-                        help='dry run')
+    parser = argparse.ArgumentParser(description="Empty directory")
+    parser.add_argument("path", metavar="path", type=str, nargs="+", help="path to empty")
+    parser.add_argument("--trash", action="store_true", help="move files to trash")
+    parser.add_argument("--remove", action="store_true", help="remove files")
+    parser.add_argument("--days", type=int, default=30, help="days to keep files")
+    parser.add_argument("--start-date", type=str, default=None, help="start date")
+    parser.add_argument("--dry-run", action="store_true", help="dry run")
     args = parser.parse_args()
 
-    logging.basicConfig(filename='empty-directory.log', level=logging.DEBUG)
+    logging.basicConfig(filename="empty-directory.log", level=logging.DEBUG, format="%(asctime)s %(message)s")
     logging.getLogger().addHandler(logging.StreamHandler())
 
-    paths = glob.glob(args.path[0] + '/**')
+    paths = glob.glob(args.path[0] + "/**")
 
-    start_date = datetime.strptime(args.start_date, '%Y-%m-%d') if args.start_date else None
+    start_date = datetime.strptime(args.start_date, "%Y-%m-%d") if args.start_date else None
 
     for path in paths:
         date_time = datetime.fromtimestamp(os.path.getmtime(path))
@@ -39,11 +33,11 @@ def main():
         if args.dry_run:
             continue
         elif args.trash:
-            subprocess.run(['trash', path])
+            subprocess.run(["trash", path])
         elif args.remove:
             os.remove(path)
         else:
-            raise ValueError('No action specified')
+            raise ValueError("No action specified")
 
 
 if __name__ == "__main__":
